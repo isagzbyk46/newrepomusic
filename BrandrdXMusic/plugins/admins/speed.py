bfa43ceb2ba863c9bf50c860c4a5203cbf0b728e
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2025-03-28 03:42:01 +0300

    Update speed.py

--- BrandrdXMusic/plugins/admins/speed.py
+++ BrandrdXMusic/plugins/admins/speed.py
@@ -14,7 +14,7 @@ checker = []
 
 
 @app.on_message(
-    filters.command(["cspeed", "speed", "cslow", "slow", "playback", "cplayback"])
+    filters.command(["cspeed", "hızlan", "speed", "cslow", "slow", "playback", "cplayback"])
     & filters.group
     & ~BANNED_USERS
 )
